9f0f668f038e3416239a76bacc9e64c02b5092fa
Kenji Nakamura <kenji.nkmr.1117@gmail.com>
2025-06-29 08:46:46 +0900

    ビルド

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,21 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        fightersGuide: resolve(__dirname, 'fighters-guide.html'),
+        hawksGuide: resolve(__dirname, 'hawks-guide.html'),
+        marinesGuide: resolve(__dirname, 'marines-guide.html'),
+        eaglesGuide: resolve(__dirname, 'eagles-guide.html'),
+        buffaloesGuide: resolve(__dirname, 'buffaloes-guide.html'),
+        lionsGuide: resolve(__dirname, 'lions-guide.html')
+      }
+    }
   },
   server: {
     port: 3000,
