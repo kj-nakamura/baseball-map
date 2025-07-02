93d205253f08131b76438d99f863b328d33614c2
Kenji Nakamura <kenji.nkmr.1117@gmail.com>
2025-07-02 19:52:51 +0900

    404解消

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,8 @@
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
-  adapter: node({
-    mode: 'standalone'
-  }),
   build: {
     assets: 'assets'
   },
